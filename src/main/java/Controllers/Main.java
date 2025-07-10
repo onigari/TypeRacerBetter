8952f950101ee3c7d8e8c6a51087b4fdcd790137
abhl05 <abhijnanpodder@gmail.com>
2025-07-10 21:27:48 +0600

    added icon

--- src/main/java/Controllers/Main.java
+++ src/main/java/Controllers/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -16,6 +17,11 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlFiles/MainMenu.fxml")));
             Scene scene = new Scene(root);
             primaryStage.setTitle("TypeRacer");
+            primaryStage.getIcons().add(
+                    new Image(Objects.requireNonNull(getClass().getResource("/img/icon.jpg")).toExternalForm())
+            );
+
+            primaryStage.setResizable(true);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch(Exception e) {
